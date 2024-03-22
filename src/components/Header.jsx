af95d7b4aa143e9bd8b5116b1502b23b58047a21
muqaddasazahra <muqaddasazahra@gmail.com>
2024-03-22 11:50:17 +0500

    Added Cart Context and showed number of cart items on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
+import { useContext } from "react"
 import logo from "../assets/logo.jpg"
 import Button from "./UI/Button"
+import CartContext from "../store/CartContext"
 export default function Header()
-{
+{   
+    const cartctx=useContext(CartContext);
+    const totalCartItems=cartctx.items.reduce((totalnumberofitems,item)=>{
+        return totalnumberofitems+item.quantity;
+    },0)
     return <header id="main-header">
         <div id="title">
             <img src={logo} alt="FoodPoint Logo" />
             <h1>FoodPoint</h1>
         </div>
         <nav>
-           <Button>Cart (0)</Button>
+           <Button textonly>Cart ({totalCartItems}) </Button>
         </nav>
     </header>
 }
---
+++ src/store/CartContext.jsx
@@ -0,0 +1,69 @@
+import { createContext, useReducer } from "react";
+
+const CartContext=createContext({
+    items:[],
+    addItem: (item)=>{},
+    removeItem:(id)=>{}
+})
+function cartReducer(state, action)
+{
+    if(action.type==="ADD_ITEM")
+    {   
+        const isExistsAlreadyIndex= state.items.findIndex((item)=> item.id===action.item.id);
+        
+        const allitems=[...state.items];
+        if(isExistsAlreadyIndex>-1)
+        {  
+            const existingItem={...state.items[isExistsAlreadyIndex]}
+           const updatedItem= {...existingItem, quantity: existingItem.quantity+1}
+           allitems[isExistsAlreadyIndex]=updatedItem;
+        } 
+        else allitems.push({...action.item, quantity:1});
+        return {...state, items:allitems}
+    }
+
+    if(action.type==="REMOVE_ITEM")
+    {
+        const isItemExistIndex=state.items.findIndex((item)=> item.id===action.id);
+        const allitems=[...state.items]
+        if(isItemExistIndex>-1)
+        {
+        const existingItem={...state.items[isItemExistIndex]};
+        if(existingItem.quantity===1)
+        {
+        allitems.slice(isItemExistIndex,1);
+        }
+        else
+        {
+            const updatedItem={
+                ...existingItem,
+                quantity: existingItem.quantity-1
+            }
+            allitems[isItemExistIndex]=updatedItem
+        }
+        }
+    
+        return {...state, items:allitems}
+    }
+    
+}
+export function CartContextProvider({children})
+{   
+    const [cart, dispatchCartActions]=useReducer(cartReducer, {items:[]})
+
+    
+
+    function addItem(item)
+    {
+        dispatchCartActions({type: "ADD_ITEM", item })
+    }
+    function removeItem(id)
+    {
+        dispatchCartActions({type:"REMOVE_ITEM", id})
+    }
+    const cartcontext={ items: cart.items, addItem, removeItem};
+    console.log(cartcontext);
+    return <CartContext.Provider value={cartcontext}>{children}</CartContext.Provider>
+}
+
+export default CartContext;
